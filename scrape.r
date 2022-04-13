7b93fa3b0484b9bb83127d9703e7a1507827702e
kenny613 <62478149+kenny613@users.noreply.github.com>
2022-04-13 12:35:25 +0200

    Add files via upload

---
+++ scrape.r
@@ -0,0 +1,11 @@
+
+library(worldfootballR)
+
+
+
+ 
+
+big_5_valuations <- get_player_market_values(country_name = c("England", "Spain", "France", "Italy", "Germany"),
+                                             start_year = 2021)
+x <- 1 
+x
